e6ffddb5b865660015aaae5ac9424a3598e1745e
Kelsco <kka245@gmail.com>
2024-11-18 01:00:34 -0500

    css added

--- puppyBowl/src/components/AllPlayers.jsx
+++ puppyBowl/src/components/AllPlayers.jsx
@@ -2,6 +2,11 @@ import { fetchPlayers } from "../API/index.js";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+<form >
+
+</form>
+
+
 export default function AllPlayers({puppyID, setPuppyId}) {
     const navigation = useNavigate();
     const [players, setPlayers] = useState([]);
@@ -17,20 +22,24 @@ console.log("players: ", players);
 
 return (
     <div>
-
+        <h2>  All Pupper Dogs</h2>
+        <ul className="pupperList">
       {players.length ? (
         players.map((player) => (
-          <div key={player.id} onClick={(e) => {
+          <li className="pupper">
+            <div key={player.id} onClick={(e) => {
             setPuppyId(player.id)
             navigation("/player")
           }}>  
             <h4>{player.name}</h4>
             <img id="dogimg" src={player.imageUrl} alt={player.name}/>
+            <div className="breed">{player.breed}</div>
           </div>
+          </li>
         ))
       ) : (
         <p>No players available</p> 
-      )}
+      )}</ul>
     </div>
   );
 }
